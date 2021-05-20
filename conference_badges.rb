ca32811b95438e272e07a9436a4396ce1637352b
Nick Flores-Tober <nflores161@gmail.com>
2021-05-20 00:49:02 -0400

    commit tree b125c2181299b8bdcb56fc2f9848f1c49dfe46e0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end  
+
+
+def batch_badge_creator(array_of_names)
+  array_of_names.map do |name|
+    "Hello, my name is #{name}."
+  end  
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index(1) do |attendee, room_numb|
+    "Hello, #{attendee}! You'll be assigned to room #{room_numb}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+  end 
+end
